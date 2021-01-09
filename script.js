e8da36938a6cdcf6d566469d4b7fb9e33b88d725
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-09 16:08:03 -0500

    Added user input varisbles

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+//User input variable
+var enter;
+var confirmNumber;
+var confirmSymbol;
+var confirmUppercase;
+var confirmLowercase;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,17 +15,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   var upperCase = document.querySelector("#upperCase");
   var lowerCase = document.querySelector("#upperCase");
-  var upperCase = document.querySelector("#upperCase");
-  var upperCase = document.querySelector("#upperCase");
+  var symbol = document.querySelector("#symbol");
+  var number= document.querySelector("#number");
   passwordText.value = password;
 }
 
 
 //Possible password characters
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numbers  = "1234567890";
-var symbols = "!@#$%^&*()_+[]{}<>/.";
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers  = ["1","2","3","4","5","6","7","8","9","0"];
+var symbols = ["!","@","#","$","%","^","&","*","()","_","+","[]","{}","<>","/","."];
 
 //Loop to choose password chracters
 for(var i=0; i<= complexity; i++){
