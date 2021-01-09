a0694be11ec6badde1c66cf99c8301e722e6df7f
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-09 16:55:12 -0500

    Added password input

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 //User input variable
-var enter;
+var passwordLength;
 var confirmNumber;
 var confirmSymbol;
 var confirmUppercase;
 var confirmLowercase;
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Possible password characters
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers  = ["1","2","3","4","5","6","7","8","9","0"];
+var symbols = ["!","@","#","$","%","^","&","*","()","_","+","[]","{}","<>","/","."];
+
+
+// Password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+//Start function to generate password
+ function generatePassword() {
+   enter = parseInt(prompt("How many characters would you like in your password? You may choose between 8 - 128. "));
+   if ( enter < 8 || enter > 128) {
+     enter = parseInt(prompt(""));
+
+   } else {
+     confrimNumber = confirm("Wiil you password contain numbers?");
+     confrimSymbol= confirm("Wiil you password contain symbols?");
+     confrimUpperCase= confirm("Wiil you password contain Uppercase letters?");
+     confirmLowercase= confirm("Wiil you password contain Lowercase letters?");
+   };
+ 
+  }
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -20,15 +60,10 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
-//Possible password characters
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers  = ["1","2","3","4","5","6","7","8","9","0"];
-var symbols = ["!","@","#","$","%","^","&","*","()","_","+","[]","{}","<>","/","."];
+var generateBtn = document.querySelector("#generate");
 
 //Loop to choose password chracters
-for(var i=0; i<= complexity; i++){
+for (var i=0; i<= complexity; i++){
   password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
 }
 
@@ -36,5 +71,5 @@ for(var i=0; i<= complexity; i++){
 document.querySelector("yourSecurePassword").value = password;
 
 
-// Add event listener to generate button
-generate.addEventListener("click", )
+
+
