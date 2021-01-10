02333bd34647f78533bcdeb9855edb5da8622410
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-09 20:26:24 -0500

    Removed syntax errors

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ var confirmLowercase;
 var userInput;
 
 //Possible password characters
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers  = ["1","2","3","4","5","6","7","8","9","0"];
 var symbols = ["!","@","#","$","%","^","&","*","()","_","+","[]","{}","<>","/","."];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Password input
 function writePassword() {
@@ -102,15 +102,7 @@ generateBtn.addEventListener("click", writePassword);
   // Random selection for loop
   for (var i = 0; i < passwordLength; i++) {
     var allChoices = userInput[Math.floor(Math.random() * userInput.length)];
-    passwordBlank.push(allChoices);
   }
-
-
-
-
-
-
-
 
    // Copy Button
    var copy = document.querySelector("#copy");
