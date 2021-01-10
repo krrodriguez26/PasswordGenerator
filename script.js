031d1f56f4a6ac05bbe875eca753198a7f7a9a73
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-01-09 20:04:41 -0500

    Added If /Else statements

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var confirmNumber;
 var confirmSymbol;
 var confirmUppercase;
 var confirmLowercase;
+var userInput;
 
 //Possible password characters
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -16,7 +17,7 @@ var symbols = ["!","@","#","$","%","^","&","*","()","_","+","[]","{}","<>","/","
 
 // Password input
 function writePassword() {
-  var password = generatePassword();
+ var password = generatePassword();
  var passwordText = document.querySelector("#password");
   
  passwordText.value = password;
@@ -29,12 +30,12 @@ generateBtn.addEventListener("click", writePassword);
  function generatePassword() {
    passwordLength = prompt("How many characters would you like in your password?");
   
-   if(!Passwordlength){
+   if(!PasswordLength){
      alert("You must enter a value");
    
     } else if (passwordLength < 8 || passwordLength >128){
       passwordLength = prompt("You may choose between 8 - 128.")
-      console.log("Password length " + passwordLength);
+      
     }   
      else {
      confrimNumber = confirm("Will your password contain numbers?");
@@ -42,7 +43,67 @@ generateBtn.addEventListener("click", writePassword);
      confrimUpperCase= confirm("Will your password contain Uppercase letters?");
      confirmLowercase= confirm("Will your password contain Lowercase letters?");
    };
- 
+
+   //  4 negative options
+     if (!confirmNumber && !confrimSymbol && !confrimUpperCase && !confirmLowercase){
+       userInput = alert("You must choose from the criteria!")
+   }
+
+   // If all criteria is true
+    else if (confirmNumber && confrimSymbol && confrimUpperCase && confirmLowercase){
+       userInput = numbers.concat(symbols,upperCase,lowerCase);
+    }
+
+    // If 3 choices are true
+     else if(confirmNumber && confrimSymbol && confrimUpperCase && confirmLowercase){
+      userInput = numbers.concat(symbols,upperCase);
+     }
+     else if(confirmNumber && confrimSymbol && confrimUpperCase && confirmLowercase){
+      userInput = numbers.concat(symbols,lowerCase);
+     }
+     else if(confirmNumber && confrimSymbol && confrimUpperCase && confirmLowercase){
+      userInput = numbers.concat(upperCase,lowerCase);
+     }
+     else if(confirmNumber && confrimSymbol && confrimUpperCase && confirmLowercase){
+      userInput = symbols.concat(numbers,upperCase);
+     }
+     //If 2 choices are true
+     else if(confirmNumber && confrimUpperCase){
+      userInput = numbers.concat(upperCase);
+     }
+     else if(confirmNumber && confirmLowerCase){
+      userInput = numbers.concat(lowerCase);
+     }
+     else if(confirmNumber && confrimSymbol){
+      userInput = numbers.concat(symbols);
+     }
+     else if(confirmLowercase && confrimUpperCase){
+      userInput = lowerCase.concat(upperCase);
+     }
+     else if(confirmLowercase && confrimNumber){
+      userInput = lowerCase.concat(numbers);
+     }
+     else if(confirmSymbol&& confrimNumber){
+      userInput = symbols.concat(numbers);
+     }
+     // If 1 choice is true
+     else if (confirmNumber) {
+      userChoices = numbers;
+    }
+    else if (confirmSymbol) {
+      userChoices = symbols;
+    }
+    else if (confirmUppercase) {
+      userChoices = upperCase;
+    }
+    else if (confirmLowercase) {
+      userChoices = lowerCase;
+    }
+  // Random selection for loop
+  for (var i = 0; i < passwordLength; i++) {
+    var allChoices = userInput[Math.floor(Math.random() * userInput.length)];
+    passwordBlank.push(allChoices);
+  }
 
 
 
@@ -51,9 +112,6 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-   
    // Copy Button
    var copy = document.querySelector("#copy");
    copy.addEventListener("click", function () {
